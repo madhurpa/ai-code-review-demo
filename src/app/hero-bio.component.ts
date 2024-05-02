4f8eba61c80553fb5769093f69f1af9bd8800813
madhurpa <madhurpa@cybage.com>
2024-05-02 16:27:58 +0530

    Added Angular Project

---
+++ src/app/hero-bio.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { HeroCacheService } from './hero-cache.service';
+import { FormsModule, NgModel } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-hero-bio',
+  template: `
+    <h4>{{hero.name}}</h4>
+    <ng-content></ng-content>
+    <textarea cols="25" [(ngModel)]="hero.description"></textarea>`,
+  providers: [HeroCacheService],
+  imports: [FormsModule]
+})
+
+export class HeroBioComponent implements OnInit  {
+  @Input() heroId = 0;
+
+  constructor(private heroCache: HeroCacheService) { }
+
+  ngOnInit() { this.heroCache.fetchCachedHero(this.heroId); }
+
+  get hero() { return this.heroCache.hero; }
+}
